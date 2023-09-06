5bdfc3a8992ebebdcf5466f01530897ca8827348
william lebouvier <wlebouvier@normandiewebschool.fr>
2023-09-06 17:00:33 +0200

    v1

---
+++ sgcw.js
@@ -0,0 +1,19 @@
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
+
+const background = document.querySelector(".background");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
