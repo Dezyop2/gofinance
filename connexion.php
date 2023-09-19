ce18c74af09b6571667204b0b185c82cb5f5d9c6
william lebouvier <wlebouvier@normandiewebschool.fr>
2023-09-19 09:40:14 +0200

    v4.2

--- connexion.php
+++ connexion.php
@@ -33,12 +33,15 @@ if(isset($_POST['envoi'])){
     <body class="p-3 mb-2 bg-body-tertiary text-white" style="--bs-bg-opacity: .8;">
         <header>
             <nav>
-                <img src="Plan_de_travail_2.svg" class="rounded float-end" width="5%" height="5%">
-                  <a href="Systeme de gestion comptable web.php"><button class="btn btn-outline-success me-2" type="button">GoFinance</button></a>
-                  <a href="board.php"><button class="btn btn-sm btn-outline-secondary" type="button">Tableau de bord</button></a>
-                  <a href="management.php"><button class="btn btn-sm btn-outline-secondary" type="button">Gestion</button></a>
-                  <a href="review.php"><button class="btn btn-sm btn-outline-secondary" type="button">Bilan</button></a>
-                  <a href="connexion.php"><button class="btn btn-sm btn-outline-secondary" type="button">Compte</button></a>
+                <a href="Systeme de gestion comptable web.php"><button class="btn btn-outline-success me-2" type="button">GoFinance</button></a>
+                <a href="board.php"><button class="btn btn-sm btn-outline-secondary" type="button">Tableau de bord</button></a>
+                <a href="management.php"><button class="btn btn-sm btn-outline-secondary" type="button">Gestion</button></a>
+                <a href="review.php"><button class="btn btn-sm btn-outline-secondary" type="button">Bilan</button></a>
+                <a href="connexion.php"><button class="btn btn-sm btn-outline-secondary" type="button">Compte</button></a>
+                <div class="float-end d-flex">
+                    <p class="mb-auto">pseudo</p>
+                    <img src="Plan_de_travail_2.svg" class="rounded">
+                </div>
             </nav>
         </header>
 
